71eddc67bb9a6afd9939db6880f26baa44349638
yamanerkam <erkamyaman35@gmail.com>
2024-09-01 17:32:40 +0300

    about-page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
+  photo1: string = 'assets/about-me.jpg'
+  gif1: string = 'assets/gif.gif'
 }
