9893cc302630d9f00d6b8e810a444e49b4a03b4a
yamanerkam <erkamyaman35@gmail.com>
2024-09-05 14:54:57 +0300

    navbar-responsive-start

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isMobileMenu = false;
+
+  toggleDropdown() {
+    this.isMobileMenu = !this.isMobileMenu;
+  }
 
 }
